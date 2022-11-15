f145fb9afdfd6c543a78e07e96071bb8c10193b4
Evgeniy <Lymilight@gmail.com>
2022-11-15 14:24:21 +0700

    задача с днями недели

--- semenar1/zadacha2/Program.cs
+++ semenar1/zadacha2/Program.cs
@@ -1,3 +1,34 @@
 ﻿// напишите программу,которая будет выдавать название дня недели по заданному номеру.
 // 3-> среда
 // 5 -> пятница
+
+Console.WriteLine("Введите номер дня недели");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 == 1)
+{
+    Console.WriteLine("понедельник");
+}
+else if (num1 == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (num1 == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (num1 == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (num1 == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (num1 == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (num1 == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
